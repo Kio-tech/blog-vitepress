a3f81f513c8d77e767eae87cc32db2de40d03afc
商秋 <kio_soft@hotmail.com>
2024-10-22 05:52:38 +0800

    首次提交

---
+++ .vitepress/config.mjs
@@ -0,0 +1,70 @@
+import { defineConfig } from 'vitepress'
+import { set_sidebar } from "../utils/auto-gen-sidebar.mjs";	// 自动生成侧边栏
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  head: [["link", { rel: "icon", href: "/jumao.svg" }]], // 配置地址栏图标
+  title: "商秋的BLOG",
+  description: "网站描述",
+  themeConfig: {
+
+    logo: 'jumao.svg', // 配置网站左上标题旁边LOGO图标，位于public目录
+
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '首页', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' },
+      { text: '冷知识', link: '/content/冷知识/四舍五入为什么不科学' }
+    ],
+
+    sidebar: {
+      "/content/冷知识": set_sidebar("content/冷知识")  // 自动生成侧边栏，键是nav的link，set_sidebar传递的参数是相对于根路径的文件夹路径
+    
+    },
+    // sidebar: [
+    //   {
+    //     text: 'Examples',
+    //     items: [
+    //       { text: 'Markdown Examples', link: '/markdown-examples' },
+    //       { text: 'Runtime API Examples', link: '/api-examples' }
+    //     ]
+    //   },
+    //   {
+    //     text: '冷知识',
+    //     items: [
+    //       { text: '四舍五入为什么不科学', link: '/冷知识/四舍五入为什么不科学' }
+    //     ]
+    //   }
+    // ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ],
+
+    // 设置搜索框
+    search: {
+      provider: "local",
+      options: {
+        translations: {
+          button: {
+            buttonText: "搜索文档",
+            buttonAriaLabel: "搜索文档",
+          },
+          modal: {
+            noResultsText: "无法找到相关结果",
+            resetButtonTitle: "清除查询条件",
+            footer: {
+              selectText: "选择",
+              navigateText: "切换",
+            },
+          },
+        },
+      },
+    },
+
+    // 底部版权信息
+    footer: {
+      copyright: '冀ICP备2024087472号-1'
+    }
+  }
+})
---
+++ index.md
@@ -0,0 +1,28 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "商秋的BLOG"
+  text: "知识分享 经验总结"
+  tagline: 持续学习，持续进步
+  image:
+    src: /cp_boy.svg
+    alt: 背景图
+  actions:
+    - theme: brand
+      text: Markdown 示例
+      link: /markdown-examples
+    - theme: alt
+      text: API 示例
+      link: /api-examples
+
+features:
+  - title: Feature A
+    details: 特征一详细描述
+  - title: Feature B
+    details: 特征二详细描述
+  - title: Feature C
+    details: 特征三详细描述
+---
+
